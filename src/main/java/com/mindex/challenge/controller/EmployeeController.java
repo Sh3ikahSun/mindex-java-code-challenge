4fb737b463503ad033c18e508a21cdb1e0919fb6
Summer DiStefano <120622368+Sh3ikahSun@users.noreply.github.com>
2023-02-10 15:45:38 -0500

    Got task 1 working

--- src/main/java/com/mindex/challenge/controller/EmployeeController.java
+++ src/main/java/com/mindex/challenge/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.mindex.challenge.controller;
 
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,5 +35,14 @@ public class EmployeeController {
 
         employee.setEmployeeId(id);
         return employeeService.update(employee);
+    }
+
+    /** Creates a reporting structure for the employee with the given id. */
+    @GetMapping("/employee/{employeeId}/reportingStructure")
+    public ReportingStructure getEmployeeReportingStructure(@PathVariable String employeeId) {
+        // Log that the reporting structure request has been received.
+        LOG.debug("Received employee reporting structure request for id [{}]", employeeId);
+        // Create the reporting structure.
+        return employeeService.getEmployeeReportingStructure(employeeId);
     }
 }
--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -5,9 +5,9 @@ public class ReportingStructure {
     private int numberOfReports;    // Number of employees that report to the employee.
 
     /** Constructor */
-    public ReportingStructure(Employee employee) {
+    public ReportingStructure(Employee employee, int numReports) {
         this.employee = employee;
-        setNumberOfReports();
+        this.numberOfReports = numReports;
     }
 
 
@@ -17,34 +17,13 @@ public class ReportingStructure {
     /** Set employee object for this reporting structure and update the number of reports. */
     public void setEmployee(Employee employee) { 
         this.employee = employee;
-        setNumberOfReports();
     }
 
     /** Get number of reports under the employee in this reporting structure. */
     public int getNumberOfReports() { return this.numberOfReports; }
 
     /** Determine and set number of reports under the employee in this reporting structure. */
-    private void setNumberOfReports() {
-        this.numberOfReports = countDirectReports(this.employee);
-    }
-
-    /** Recursively counts number of reporting employees under the given employee. */
-    private int countDirectReports(Employee employee, List<Employee> uniqueReports) {
-        List<Employee> reports = employee.getDirectReports();
-        if (reports.isEmpty()) { 
-            return 0; 
-        } else {
-            int numReports = 0;
-            for (Employee report : reports) {
-                // If an employee is in uniqueReports, they and their lower reports have been counted already, so we skip them.
-                if (!uniqueReports.contains(report)) {
-                    // Add this reporting employee to the list of unique reports so they're not counted again.
-                    uniqueReports.add(report);
-                    // Add this reporting employee and their reporting employees to the running count.
-                    numReports = 1 + countDirectReports(report);
-                }
-            }
-            return numReports;
-        }
+    public void setNumberOfReports(int numReports) {
+        this.numberOfReports = numReports;
     }
 }
